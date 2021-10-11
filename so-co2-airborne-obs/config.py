a898c005ca06cee5624d6158ca5b301a452497c7
matt-long <mclong@ucar.edu>
2021-10-11 16:01:38 -0600

    fall back to /Users/mclong if /Users/mclong/scratch/tmp is missing

--- so-co2-airborne-obs/config.py
+++ so-co2-airborne-obs/config.py
@@ -4,16 +4,17 @@ from jinja2 import Template
 
 path_to_here = os.path.dirname(os.path.realpath(__file__))
 
+
 def _get_config_dict():
     """return the configuration dictionary with environment variables replaced"""
-    
-    with open(f'{path_to_here}/_config_calc.yml') as fid:
-        config_dict_in = yaml.safe_load(fid)    
-    
-    required_keys = ['project_tmpdir', 'model_data_dir', 'dash_asset_fname']
+
+    with open(f"{path_to_here}/_config_calc.yml") as fid:
+        config_dict_in = yaml.safe_load(fid)
+
+    required_keys = ["project_tmpdir", "model_data_dir", "dash_asset_fname"]
     for key in required_keys:
-        assert key in config_dict_in, f'config missing {key}'
-        
+        assert key in config_dict_in, f"config missing {key}"
+
     config_dict = {}
     for key, value in config_dict_in.items():
         if isinstance(value, str):
@@ -27,18 +28,18 @@ def _get_config_dict():
 # get configuration dictionary
 def get(parameter):
     config_dict = _get_config_dict()
-    
+
     derived = False
     if parameter in ["model_data_dir_root"]:
         derived = True
     else:
         assert parameter in config_dict, f"unknown parameter {parameter}"
-    
+
     if not derived:
         value = config_dict[parameter]
-    
+
     elif parameter == "model_data_dir_root":
-        model_data_dir = config_dict['model_data_dir']
+        model_data_dir = config_dict["model_data_dir"]
         value = os.path.dirname(model_data_dir)
 
     if parameter in [
@@ -47,12 +48,15 @@ def get(parameter):
         "model_data_dir_root",
     ]:
         try:
-            os.makedirs(value, exist_ok=True)    
+            os.makedirs(value, exist_ok=True)
         except OSError:
             print(f"error in config: cannot mkdir {value}")
-            TMPDIR = os.environ["TMPDIR"]
-            value = f"{TMPDIR}/so-co2-airborne-obs/{parameter}"            
+            try:
+                TMPDIR = os.environ["TMPDIR"]
+            except:
+                TMPDIR = os.environ["HOME"]
+            value = f"{TMPDIR}/so-co2-airborne-obs/{parameter}"
             print(f"setting {parameter} to {value}")
-            os.makedirs(value, exist_ok=True) 
-                                
+            os.makedirs(value, exist_ok=True)
+
     return value
